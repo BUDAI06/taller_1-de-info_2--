b659d87507c2f796306042f5e0194c394f7c3896
BUDAI06 <mauricio.vallejo1@udea.edu.co>
2025-09-23 23:41:17 -0500

    Delete Hospital_sensores directory

--- Hospital_sensores/sql/bd.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE hospital;
-USE hospital;
-
-CREATE TABLE sensores (
-    id_sensor VARCHAR(20) PRIMARY KEY,
-    tipo VARCHAR(50) NOT NULL,
-    unidad_medida VARCHAR(20) NOT NULL,
-    atributo_extra VARCHAR(100)
-);
